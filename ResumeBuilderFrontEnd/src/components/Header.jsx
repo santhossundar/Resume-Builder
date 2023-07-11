df3924b347e404ca3f4e26e61565c48f34367671
santhossundar <santhoshsunthar@gmail.com>
2023-07-11 16:43:40 +0530

    Added #Header section

---
+++ ResumeBuilderFrontEnd/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <section className='header'>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1">Resume Builder</span>
+                </div>
+            </nav>
+        </section>
+    )
+}
+
+export default Header
