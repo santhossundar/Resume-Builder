e95b5dbb4eff041811752de1b8bebeeaef0ae13b
santhossundar <santhoshsunthar@gmail.com>
2023-07-11 16:39:29 +0530

    Added #HomePage file

---
+++ ResumeBuilderFrontEnd/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Form from '../components/forms/Form';
+
+const Home = () => {
+    return (
+        <div className='home-page'>
+            <Header/>
+            <Form/>
+        </div>
+    )
+}
+
+export default Home
