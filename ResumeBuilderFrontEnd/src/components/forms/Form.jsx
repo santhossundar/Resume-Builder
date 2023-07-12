b16a10d60b0a713a748acb801f94ea11c2bfc17e
santhossundar <santhoshsunthar@gmail.com>
2023-07-12 16:44:54 +0530

    Added #Form component

---
+++ ResumeBuilderFrontEnd/src/components/forms/Form.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import FormBody from './FormBody';
+
+const Form = () => {
+    const [page, setPage] = useState(0);
+
+    const formTitles = [
+        "Personal Information", 
+        "Workplace Experiences", 
+        "Education Details",
+    ]
+
+    return (
+        <div className='form'>
+            <div className="progress-menu"></div>
+            <div className="form-container">
+                <div className="header">
+                    <div className="form-title">
+                        <h5>{formTitles[page]}</h5>
+                    </div>
+                </div>
+                <div className="body">
+                    <FormBody pageId={page}/>
+                </div>
+                <div className="footer">
+                    <button onClick={() => {
+                        setPage((currentPage) => currentPage-1);
+                        }}>
+                            Prev
+                    </button>
+                    
+                    <button onClick={() => {
+                        setPage((currentPage) => currentPage+1)
+                        }}>
+                            Next
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Form
