59e15840c3cfcd7de53866760ca5de335dfb2162
santhossundar <santhoshsunthar@gmail.com>
2023-07-13 15:35:56 +0530

    Added changes in #Form component

--- ResumeBuilderFrontEnd/src/components/forms/Form.jsx
+++ ResumeBuilderFrontEnd/src/components/forms/Form.jsx
@@ -5,34 +5,37 @@ const Form = () => {
     const [page, setPage] = useState(0);
 
     const formTitles = [
-        "Personal Information", 
-        "Workplace Experiences", 
+        "Personal Information",
+        "Workplace Experiences",
         "Education Details",
     ]
 
     return (
-        <div className='form'>
+        <div className='form m-5'>
             <div className="progress-menu"></div>
-            <div className="form-container">
+
+            <div className="form-container m-5">
                 <div className="header">
                     <div className="form-title">
                         <h5>{formTitles[page]}</h5>
                     </div>
                 </div>
+
                 <div className="body">
-                    <FormBody pageId={page}/>
+                    <FormBody pageId={page} />
                 </div>
+
                 <div className="footer">
                     <button onClick={() => {
-                        setPage((currentPage) => currentPage-1);
-                        }}>
-                            Prev
+                        setPage((currentPage) => currentPage - 1);
+                    }}>
+                        Prev
                     </button>
-                    
+
                     <button onClick={() => {
-                        setPage((currentPage) => currentPage+1)
-                        }}>
-                            Next
+                        setPage((currentPage) => currentPage + 1)
+                    }}>
+                        Next
                     </button>
                 </div>
             </div>
