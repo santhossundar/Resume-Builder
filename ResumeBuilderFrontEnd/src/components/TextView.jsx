b26ce4b2e5d4dee7b6c9be29457f93d32132dd10
santhossunthar <santhoshsunthar@gmail.com>
2023-07-20 17:55:12 +0530

    Added #TextView style changes

--- ResumeBuilderFrontEnd/src/components/TextView.jsx
+++ ResumeBuilderFrontEnd/src/components/TextView.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const TextView = () => {
     return (
-        <div className='btn w-100 btn-txt-view btn-light text-dark ms-2 me-1 pt-2 pb-2 ps-4 pe-4 shadow'>
-            Working Experience 01
+        <div className='btn w-100 btn-txt-view btn-light text-dark ms-2 me-2 pt-2 pb-2 ps-4 pe-4'>
+            <div className="row">
+                <div className="col-6 d-flex justify-content-between">Title</div>
+                <div className="col-6 d-flex justify-content-between">Organization</div>
+            </div>
         </div>
     )
 }
