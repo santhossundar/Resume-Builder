24ee4674466ef65850797fe1d62f3b5f7fd569bd
santhossunthar <santhoshsunthar@gmail.com>
2023-07-20 17:34:47 +0530

    Added major changes

--- ResumeBuilderFrontEnd/src/components/TextView.jsx
+++ ResumeBuilderFrontEnd/src/components/TextView.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const TextView = () => {
     return (
-        <div className='btn btn-txt-view btn-light ms-2 me-1 pt-2 pb-2 ps-4 pe-4 shadow'>
+        <div className='btn w-100 btn-txt-view btn-light text-dark ms-2 me-1 pt-2 pb-2 ps-4 pe-4 shadow'>
             Working Experience 01
         </div>
     )
