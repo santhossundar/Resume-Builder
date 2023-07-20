11b383e9cbb0744e588cd5c65c7e600da0e2eb4d
santhossunthar <santhoshsunthar@gmail.com>
2023-07-20 15:20:44 +0530

    Added #Button component changes

--- ResumeBuilderFrontEnd/src/components/Header.jsx
+++ ResumeBuilderFrontEnd/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <section className='header'>
+        <section className='header w-100'>
             <nav className="navbar">
                 <div className="container-fluid">
                     <span className="navbar-brand mb-0 h1">Resume Builder</span>
