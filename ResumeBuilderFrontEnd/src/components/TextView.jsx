d01d6c549404ef3db6b39f0dd65d351ca13f91f7
santhossunthar <santhoshsunthar@gmail.com>
2023-07-20 16:59:01 +0530

    Added #TextView component

---
+++ ResumeBuilderFrontEnd/src/components/TextView.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TextView = () => {
+    return (
+        <div className='btn btn-txt-view btn-light ms-2 me-1 pt-2 pb-2 ps-4 pe-4 shadow'>
+            Working Experience 01
+        </div>
+    )
+}
+
+export default TextView
